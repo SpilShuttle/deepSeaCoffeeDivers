c335770775460be10da5ebcc2b1f6c8c1e773d5f
SpilShuttle <62037675+SpilShuttle@users.noreply.github.com>
2020-03-13 23:56:27 +0200

    upload new script.js

--- js/script.js
+++ js/script.js
@@ -15,6 +15,6 @@ $(function(){
   $('.hero h1').css('animation-delay', '2.5s');
   $('.hero h4').css('animation-delay', '3s');
   $('.about-us .animated').css('animation-delay', '4s');
-  $('.projects button').css('animation-delay', '4.5s');
+  $('.projects').css('animation-delay', '4.5s');
   $('.footer').css('animation-delay', '5s');
 });
